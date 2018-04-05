4d3c20a29298de214fe6694da78e727e032fd99a
Galen Cochrane <cochgale@isu.edu>
2018-04-04 19:34:07 -0600

    simplified API

--- topics.cpp
+++ topics.cpp
@@ -1,24 +1,43 @@
 
 #include <unordered_map>
+#include <memory>
 #include "topics.hpp"
 
 namespace rtu {
   namespace topics {
 
     namespace {
+
+      struct EitherAction {
+          bool isSimple;
+          union {
+              Action action;
+              SimpleAction simpleAction;
+          };
+          EitherAction() { } // DANGER
+      };
+
       struct Topic {
-        std::unordered_map<uint32_t, Action> subscriptions;
+        std::unordered_map<uint32_t, EitherAction> subscriptions;
         void publish(void* data) {
           for (auto sub : subscriptions) {
-            sub.second(data);
+            sub.second.isSimple ? sub.second.simpleAction() : sub.second.action(data);
           }
         }
       };
       std::unordered_map<std::string, Topic> activeTopics;
+
     }
 
-    Subscription::Subscription(std::string topic, Action action) : topic(topic), id(++nextId) {
-      activeTopics[topic].subscriptions[id] = action;
+    Subscription::Subscription(const std::string &topic, const Action &action) : topic(topic), id(++nextId) {
+      activeTopics[topic].subscriptions[id].action = action;
+      activeTopics[topic].subscriptions[id].isSimple = false;
+      printf("SUB: %s %u\n", topic.c_str(), id);
+    }
+
+    Subscription::Subscription(const std::string &topic, const SimpleAction &simpleAction) : topic(topic), id(++nextId){
+      activeTopics[topic].subscriptions[id].simpleAction = simpleAction;
+      activeTopics[topic].subscriptions[id].isSimple = true;
       printf("SUB: %s %u\n", topic.c_str(), id);
     }
 
@@ -29,8 +48,12 @@ namespace rtu {
 
     std::atomic<uint32_t> Subscription::nextId(0);
 
-    void publish(std::string topic, void* data) {
+    void publishPtr(const std::string &topic, void *data) {
       activeTopics[topic].publish(data);
+    }
+
+    void publish(const std::string& topic) {
+      activeTopics[topic].publish(nullptr);
     }
 
   }
--- topics.hpp
+++ topics.hpp
@@ -2,15 +2,36 @@
 
 #include <string>
 #include <atomic>
+#include <memory>
 #include "delegate.hpp"
+
+// Get a std::unique_ptr to a subscription without all the syntax
+#define RTU_MAKE_SUB_UNIQUEPTR(tpc, fnc, inst) std::make_unique<Subscription>(tpc, RTU_MTHD_DLGT(&fnc, inst));
 
 namespace rtu {
   namespace topics {
-    void publish(std::string topic, void* data);
+
+    void publishPtr(const std::string &topic, void *data);
+
+    void publish(const std::string& topic);
+
+    template <typename DataType>
+    void publish(const std::string& topic, const DataType&& data) {
+      publishPtr(topic, (void *) &data);
+    }
+
+    template <typename DataType>
+    void publish(const std::string& topic, const DataType& data) {
+      publishPtr(topic, (void *) &data);
+    }
+
     typedef Delegate<void(void*)> Action;
+    typedef Delegate<void()> SimpleAction;
+
     class Subscription {
       public:
-        Subscription(std::string topic, Action action);
+        Subscription(const std::string &topic, const Action &action);
+        Subscription(const std::string &topic, const SimpleAction &simpleAction);
         Subscription(const Subscription&) = delete; // no copies
         Subscription& operator=(const Subscription&) = delete; // no self-assignments
         Subscription(Subscription&&) = delete; // no moves
@@ -21,5 +42,6 @@ namespace rtu {
         uint32_t id;
         static std::atomic<uint32_t> nextId;
     };
+
   }
 }
